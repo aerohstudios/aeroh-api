2ba905b69a9ada40a905dc96518da2f20ebfa2df
Shiv Deepak Muddada <shiv@aeroh.org>
2023-01-25 12:04:52 -0800

    add schema url for aeroh cloud api in swagger ui

--- docs/cloud/v1/swagger-initializer.js
+++ docs/cloud/v1/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://aerohstudios.github.io/aeroh-api/cloud/v1/schema.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
